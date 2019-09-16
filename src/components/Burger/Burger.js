4d0635c20be1e08a39a0bc0d9736ed345da41eca
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2019-09-16 16:27:08 +0300

    Added data checking for Burger ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,12 +6,19 @@ import BurgerIngredients from './BurgerIngredient/BurgerIngredients'
 
 const Burger = (props) => {
 
-    const transformIngred = Object.keys(props.ingredients)
+    let transformIngred = Object.keys(props.ingredients)
         .map( ingred => {
             return [...Array(props.ingredients[ingred])].map( (_, index) => {
                 return <BurgerIngredients key={ingred + index} type={ingred} />
             })
         })
+        .reduce( (arr, current) => {
+            return arr.concat(current)
+        }, [])
+
+    if (transformIngred.length === 0) {
+        transformIngred = <p>Please start adding ingredients!</p>
+    }
     console.log(transformIngred)
     return(
         <div className={classes.Burger}>
